32f1230fe371e7fbae75d570a116b34ea88eaef4
İbrahim Can Mercan <mrcnn77@gmail.com>
2020-11-05 16:29:32 +0300

    Update getUsername.js

--- getUsername.js
+++ getUsername.js
@@ -2,6 +2,6 @@ const fetch  = require('node-fetch');
 
 module.exports = link => {
 	return fetch(link, { method: 'GET' })
-	.then(res => res.status === 404 ? false : true)
+	.then(res => res.status !== 404 ? true : false)
 	.catch(console.error);
 };
