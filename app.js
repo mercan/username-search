30b6e41b70ad2c179c9999566475110e28c2be7e
İbrahim Can Mercan <imrcn77@gmail.com>
2021-06-25 16:08:15 +0300

    Update app.js

--- app.js
+++ app.js
@@ -3,14 +3,14 @@ const app = require('express')();
 // API Require
 const pinterestSearch = require('./API/pinterest');
 const instagramSearch = require('./API/instagram');
-const dokuzGagSearch  = require('./API/dokuzGAG');
-const dribbbleSearch  = require('./API/dribbble');
-const wattpadSearch   = require('./API/wattpad');
-const tumblrSearch    = require('./API/tumblr');
-const githubSearch    = require('./API/github');
-const redditSearch    = require('./API/reddit');
-const mediumSearch    = require('./API/medium');
-const quoraSearch     = require('./API/quora');
+const dokuzGagSearch = require('./API/dokuzGAG');
+const dribbbleSearch = require('./API/dribbble');
+const wattpadSearch = require('./API/wattpad');
+const tumblrSearch = require('./API/tumblr');
+const githubSearch = require('./API/github');
+const redditSearch = require('./API/reddit');
+const mediumSearch = require('./API/medium');
+const quoraSearch = require('./API/quora');
 const allSearch = require('./API/allSearch');
 
 // API App.use
